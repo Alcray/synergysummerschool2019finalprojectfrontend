4fa399cce84bbfbc0a040979b89915b857b6fd83
Aram Hovsepyan <hayrapetyan.alexan@gmail.com>
2019-08-23 13:39:22 +0400

    SortByFaculty

--- pages/Student/student.controller.js
+++ pages/Student/student.controller.js
@@ -1,7 +1,7 @@
 function getJSONfromDB(){
     return [
         {
-            firstName: 'Roger', lastName: 'Barber', faculty: 'Computer Science',
+            firstName: 'Roger', lastName: 'Barber', faculty: 'Law',
             subjects: [
                 {subject:'Combinatorics', marks: 20},
                 {subject:'Graph Theory', marks: 17},
@@ -9,7 +9,7 @@ function getJSONfromDB(){
             ]
         },
         {
-            firstName: 'Roger', lastName: 'Barber', faculty: 'Computer Science',
+            firstName: 'Roger', lastName: 'Barber', faculty: 'ComputerScience',
             subjects: [
                 {subject:'Combinatorics', marks: 20},
                 {subject:'Graph Theory', marks: 17},
@@ -17,7 +17,7 @@ function getJSONfromDB(){
             ]
         },
         {
-            firstName: 'Aaran', lastName: 'Barber', faculty: 'Computer Science',
+            firstName: 'Aaran', lastName: 'Barber', faculty: 'ComputerScience',
             subjects: [
                 {subject:'Combinatorics', marks: 20},
                 {subject:'Graph Theory', marks: 17},
@@ -25,7 +25,7 @@ function getJSONfromDB(){
             ]
         },
         {
-            firstName: 'Roger', lastName: 'Barber', faculty: 'Computer Science',
+            firstName: 'Roger', lastName: 'Barber', faculty: 'ComputerScience',
             subjects: [
                 {subject:'Combinatorics', marks: 20},
                 {subject:'Graph Theory', marks: 17},
@@ -66,21 +66,64 @@ function updateResult(query) {
     window.DataObject.map(function(algo){
         query.split(" ").map(function (word){
             if((algo.firstName + ' ' + algo.lastName).toLowerCase().indexOf(word.toLowerCase()) != -1){
+
+              let x = 0;
+              let l = 0;
+              for (let mark of algo.subjects) {
+                  x += mark.marks;
+              }
+              if (l != 0) x = x / l;
+
                 res.innerHTML += `<div class="divTableRow">
                 <div class="divTableCell1">
-                    <div style="display: flex;align-items: center;width: 100%">
+                    <div class="autocomp-div" >
                         <svg class="icon icon-triangle-right" style="width: 20px;height: 20px;" onclick="popUpBox()">
                             <use xlink:href="#icon-triangle-right"></use>
                         </svg>
                         <p style="margin-left: 15px;">${algo.firstName} ${algo.lastName}</p>
                     </div>
                 </div>
-                <div class="divTableCell2">${algo.faculty}</div>
+                <div class="divTableCell2">${x}</div>
+            </div>`;
+            }
+        })
+    })
+    let autocomp = document.getElementsByClassName("autocomp-div")[0].querySelector('p').innerHTML;
+    alert(autocomp);
+}
+
+
+function sortByFaculty() {
+    let res = document.getElementById("tableBody");
+
+
+    let query = document.getElementById("sortByFaculty").value;
+res.innerHTML = "";
+    window.DataObject.map(function(algo){
+        query.split(" ").map(function (word){
+            if(algo.faculty.toLowerCase() == query || query == "all"){
+              let x = 0;
+                let l = 0;
+                for (let mark of algo.subjects) {
+                    x += mark.marks;
+                }
+                if (l != 0) x = x / l;
+                res.innerHTML += `<div class="divTableRow">
+                <div class="divTableCell1">
+                    <div class="autocomp-div" >
+                        <svg class="icon icon-triangle-right" style="width: 20px;height: 20px;" onclick="popUpBox()">
+                            <use xlink:href="#icon-triangle-right"></use>
+                        </svg>
+                        <p style="margin-left: 15px;">${algo.firstName} ${algo.lastName}</p>
+                    </div>
+                </div>
+                <div class="divTableCell2">${x}</div>
             </div>`;
             }
         })
     })
 }
+
 
 function popUpBox(){
     var modal = document.getElementById("myModal");
@@ -91,11 +134,10 @@ function popUpBox(){
     span.onclick = function() {
         modal.style.display = "none";
     }
-    
+
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
     }
 }
-
