a161eb0f12d397387d7bfdd990a9b4aef37b0c11
user <user@example.com>
2019-08-23 10:19:30 +0400

    a

---
+++ pages/Student/student.controller.js
@@ -0,0 +1,13 @@
+function getJSONfromDB(){
+    return [
+        {firstName, lastName, },
+        {},
+        {},
+        {},
+        {}
+    ];
+}
+function LoadDB(){
+    let JSONText = getJSONfromDB();
+    let DataObject = JSON.parse(JSONText);
+}
