2b813124d181d3c8bb9fb1bf236fc6f56eb4d453
user <user@example.com>
2019-08-23 11:32:46 +0400

    as

--- pages/Student/student.controller.js
+++ pages/Student/student.controller.js
@@ -11,3 +11,23 @@ function LoadDB(){
     let JSONText = getJSONfromDB();
     let DataObject = JSON.parse(JSONText);
 }
+
+
+
+function popUpBox(){
+    var modal = document.getElementById("myModal");
+
+    var span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
